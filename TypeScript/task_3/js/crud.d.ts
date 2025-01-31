f0e1eb96ac47a68c4d114faa992bf9634a2ede5a
scotty800 <scottyndanga02400@gmail.com>
2025-01-31 11:28:24 +0100

    Ambient Namespaces

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+declare module "crud" {
+    export function insertRow(row: RowElement): RowID;
+    export function deleteRow(rowId: RowID): void;
+    export function updateRow(rowId: RowID, row: RowElement): RowID;
+  }
